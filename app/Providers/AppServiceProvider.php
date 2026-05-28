601d253d9cde5c01c5d82edba8a1ef1b3dfdd339
icangg05 <ilmifaizan2002@gmail.com>
2026-05-28 11:49:56 +0800

    updated app

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,29 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
+  /**
+   * Register any application services.
+   */
+  public function register(): void
+  {
+    //
+  }
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        //
+  /**
+   * Bootstrap any application services.
+   */
+  public function boot(): void
+  {
+    // Force HTTPS in production
+    $isHttps = request()->isSecure() || request()->header('x-forwarded-proto') === 'https';
+
+    if (config('app.env') === 'production' || $isHttps) {
+      URL::forceScheme('https');
     }
+  }
 }
